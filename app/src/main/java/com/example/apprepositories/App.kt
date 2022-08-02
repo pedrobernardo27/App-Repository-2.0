766d4382ded11bffa18ef789bd7d97771d1a993c
Pedro <josepedrobernardo@hotmail.com>
2022-08-02 17:57:17 -0300

    criação de app de repositórios do gitHub

---
+++ app/src/main/java/com/example/apprepositories/App.kt
@@ -0,0 +1,23 @@
+package com.example.apprepositories
+
+import android.app.Application
+import com.example.apprepositories.data.di.DataModule
+import com.example.apprepositories.domain.di.DomainModule
+import com.example.apprepositories.presentation.di.PresentationModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@App)
+        }
+
+        DataModule.load()
+        DomainModule.load()
+        PresentationModule.load()
+    }
+}
